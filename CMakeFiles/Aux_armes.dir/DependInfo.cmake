ffaab954b612439bf80667c7f720f9079d32e03b
Loïc Berthelot <loic.berthelot49@gmail.com>
2023-04-05 11:16:12 +0200

    premiere_compilation

---
+++ CMakeFiles/Aux_armes.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/etud/Documents/stage/aux-armes/carte.cpp" "/home/etud/Documents/stage/aux-armes/CMakeFiles/Aux_armes.dir/carte.cpp.o"
+  "/home/etud/Documents/stage/aux-armes/jeu.cpp" "/home/etud/Documents/stage/aux-armes/CMakeFiles/Aux_armes.dir/jeu.cpp.o"
+  "/home/etud/Documents/stage/aux-armes/joueur.cpp" "/home/etud/Documents/stage/aux-armes/CMakeFiles/Aux_armes.dir/joueur.cpp.o"
+  "/home/etud/Documents/stage/aux-armes/main.cpp" "/home/etud/Documents/stage/aux-armes/CMakeFiles/Aux_armes.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "jeu.h"
+
+int main() {
+    std::cout<<"Hello World !"<<std::endl;
+    return 0;
+}
