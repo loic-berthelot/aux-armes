6b672ccfe5194f6fe204a0c60baf3f95b382cea9
Lakyous <giovannicarre49@gmail.com>
2023-04-25 14:09:19 +0000

    fd

--- decisions/IA.cpp
+++ decisions/IA.cpp
@@ -2,6 +2,8 @@
 
 void IA::jouerArmee(Carte & carte) {
     std::shared_ptr<Armee> armee = carte.getArmee();
-    for (unsigned int i = 0; i < armee->taille(); i++) 
-      armee->donnerOrdre(i, std::make_shared<Ordre>(TypeOrdre::DEPLACER, 0, 0));
+    for (unsigned int i = 0; i < armee->taille(); i++) {
+      std::pair<int,int> pos = carte.positionAleatoireCarte();
+      armee->donnerOrdre(i, std::make_shared<Ordre>(TypeOrdre::DEPLACER, pos.first, pos.second));
+    }
 }
